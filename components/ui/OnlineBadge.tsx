55c4911e562c628ef525d7bd3506e9322dca669d
LALASON Annael <ralalanael@gmail.com>
2024-07-10 14:30:35 +0300

    ADD text rotator for improving visual design

--- components/ui/OnlineBadge.tsx
+++ components/ui/OnlineBadge.tsx
@@ -2,26 +2,18 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 
 const Badge = ({ className }: { className: string }) => {
-	return (
-		<div className={`flex items-center space-x-2 ${className}`}>
-			<div className="relative">
-				<Image
-					src="/profile.jpg" // Assurez-vous d'avoir une image de profil à cette source
-					alt="Profile"
-					className="w-12 h-12 rounded-full"
-					width={100}
-					height={100}
-				/>
-				<motion.div
-					initial={{ scale: 0 }}
-					animate={{ scale: 1 }}
-					transition={{ duration: 0.3 }}
-					className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-white rounded-full"
-				/>
-			</div>
-			<span className="text-sm font-medium text-gray-700">Available</span>
-		</div>
-	);
+    return (
+        <div
+            className={`flex items-center bg-white-100/30 backdrop:blur-xl px-2 py-1 rounded-lg space-x-2 ${className}`}>
+            <motion.div
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 0.3 }}
+                className=" w-3 h-3 bg-green-500 border-2 border-white rounded-full"
+            />
+            <span className="text-sm font-medium text-slate-50">Available</span>
+        </div>
+    );
 };
 
 export default Badge;
---
+++ components/ui/TextRotator.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function TextRotator() {
+    return (
+        <div className="py-4  rounded-md flex flex-col justify-center items-center overflow-hidden">
+            <div className="md:tracking-wider mb-4 text-md md-text-lg lg:text-xl">
+                I am a UI/UX Designer &
+                <span className="inline-flex ml-2 flex-col h-[calc(theme(fontSize.lg)*theme(lineHeight.tight))] sm:h-[calc(theme(fontSize.3xl)*theme(lineHeight.tight))] overflow-hidden">
+                    <ul className="block font-semi-bold text-left font-rubik text-lg sm:text-3xl leading-tight [&_li]:block animate-text-slide">
+                        <li className="text-fuchsia-600">Freelancer</li>
+                        <li className="text-fuchsia-600">Developer</li>
+                        <li className="text-fuchsia-600">Gamer</li>
+                        <li className="text-fuchsia-600">Creator</li>
+                        <li className="text-fuchsia-600">Student</li>
+                        <li className="text-fuchsia-600">Contributer</li>
+                    </ul>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default TextRotator;
